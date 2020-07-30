7abcd29aef6e86d7a65d71a244b34a09bf4e80f1
damephena <okoyeifenna24@gmail.com>
2020-07-30 10:31:02 +0100

    change page size

--- procure/settings.py
+++ procure/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
 
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 30
     # 'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
